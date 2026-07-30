d962c3a37bff8a9abb216b50c62742a5525f7af3
Reddypogu Vamsi <reddypoguvamsi10@gmail.com>
2026-07-30 22:30:35 +0530

    Added HelloWorld program
    git commit --amend --reset-author#:q!
    :q!
    :wq

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the java Track.");
+        System.out.println("I already know how to build programming logic.");
+        System.out.println("now i am learning to express that logic using java.");//Write your code here
+    }
+}
