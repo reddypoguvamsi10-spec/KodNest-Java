7b744b424cc6a7c21edbf58a9174528a4611894f
Reddypogu Vamsi <reddypoguvamsi10@gmail.com>
2026-08-06 12:00:09 +0530

    Day5Changes

--- track/M02-introduction to java\T01-introduction to java and programming\M01-T01-ST01-L-First java program\work/First.java/NestedLoop1.java
+++ track/M02-introduction to java\T01-introduction to java and programming\M01-T01-ST01-L-First java program\work/First.java/NestedLoop1.java
@@ -2,9 +2,9 @@
 class NestedLoop1 {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-                System.out.print(j);
+                System.out.println(j);
             }
             System.out.println();
         }
