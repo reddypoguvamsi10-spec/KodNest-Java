7cc7733a987ab7e40db71e09665d332be137cdb5
Reddypogu Vamsi <reddypoguvamsi10@gmail.com>
2026-08-06 12:48:46 +0530

    Day5Changes

---
+++ track/M02-introduction to java\T01-introduction to java and programming\M01-T01-ST01-L-First java program\work/First.java/NestedContinue.java
@@ -0,0 +1,15 @@
+
+class NestedContinue {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j == 3) {
+                    continue;
+                }
+                System.out.println("i :" + i + " j :" + j);
+            }
+            System.out.println();
+        }
+    }
+}
